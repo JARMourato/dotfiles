8b1875aa475ad9dc0498438d00ffea531a00dd8b
JARMourato <joao.armourato@gmail.com>
2022-10-11 17:28:01 +0100

    Sync dotfiles

--- Scripts/dotfiles_sync_with_remote.sh
+++ Scripts/dotfiles_sync_with_remote.sh
@@ -8,4 +8,9 @@ cd ~/.dotfiles
 hub sync
 
 # For now this will suffice
-./_set_up.sh
+
+source .exports
+source Scripts/set_up_symlinks.sh
+source Scripts/set_up_dependencies.sh
+source .zshrc # Must be run again after installing dependencies to apply changes
+source Scripts/set_up_user_defaults.sh
--- _set_up.sh
+++ _set_up.sh
@@ -5,10 +5,10 @@
 ################################################################################
 
 source .exports
-source set_up_symlinks.sh
-source set_up_dependencies.sh
+source Scripts/set_up_symlinks.sh
+source Scripts/set_up_dependencies.sh
 source .zshrc # Must be run again after installing dependencies to apply changes
-source set_up_user_defaults.sh
+source Scripts/set_up_user_defaults.sh
 source Terminal/set_up_terminal.sh
-source set_up_machine_specific_settings.sh
-source clean_up.sh
+source Scripts/set_up_machine_specific_settings.sh
+source Scripts/clean_up.sh
