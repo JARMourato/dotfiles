1ad2d571475cd08cc1fafc489d50b9d571adbed6
JARMourato <joao.armourato@gmail.com>
2022-10-18 01:15:36 +0100

    Sync dotfiles

--- Xcode/set_up_xcode.sh
+++ Xcode/set_up_xcode.sh
@@ -4,6 +4,6 @@
 ### Installs Xcode Code Snippets and Solarized Dark Theme
 ################################################################################
 
-HEADER_TEMPLATE_DIR="$HOME/Library/Developer/Xcode/UserData/"
+HEADER_TEMPLATE_DIR="$HOME/Library/Developer/Xcode/UserData"
 
 cp $DOTFILES_DIR/Xcode/IDETemplateMacros.plist $HEADER_TEMPLATE_DIR/IDETemplateMacros.plist
